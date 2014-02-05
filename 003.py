a29e09851d06a6e3228515b03ac14ace1b688175
Tuan Huynh <tuanlh@gmail.com>
2014-02-05 00:30:21 -0800

    Optimize the isPrime by checking up to sqrt(n), not n/2

--- 003.py
+++ 003.py
@@ -8,51 +8,59 @@
 # Answer:
 # 6857
 
-def isPrime(n):
-  nHalf = n / 2   # is this accurate?
+import math
 
-  print "Is " + `n` + " a prime number??"
+
+def isPrime(n):
+  print "  check if " + `n` + " is prime"
+  # Altho sqrt calc is expensive, using n/2 is worse
+  sqrtN = int(math.ceil(math.sqrt(n)))
 
   if (n == 2):
     return True
 
   if (n % 2 == 0):
-    #print "No, its even."
     return False
 
-  for i in range(3, nHalf, 2):
+  for i in range(3, sqrtN, 2):
     if (n % i == 0):
       return False
 
   return True
+
 
 def largestPrimeFactor(n):
   # loop from 2 to n/2 (i)
   # if n % i == 0, then i is LPF, then set n to n/i
   i = 2
   lpf = 1
+  nHalf = n/2
 
-  while i < n/2:
+  while i < nHalf:
+    # if i is a factor, it must be prime, since we've checked (and divided out)
+    # all factors less than i
     if (n % i == 0):
+      print `i` + " is a factor."
+      # Check if the other divisor is a prime, it'd be the largest prime factor
+      if (isPrime(n/i)):
+        lpf = n/i
+        break
+
       n = n / i
       lpf = i
     else:
       i += 1
-  return lpf
 
+  return lpf
 
 
 # ------------------------------------------------------------------------------
 def problem_003():
 
-  #lpf = largestPrimeFactor(13195)
+  lpf = largestPrimeFactor(600851475143)
 
   # `backtick` also works like repr(), when adding str to int
-  #print "Largest Prime Factor: " + str(lpf)
-
-  for i in range(1, 20):
-    if (isPrime(i)):
-      print i
+  print "Largest Prime Factor: " + str(lpf)
 
 
 # ------------------------------------------------------------------------------
